15af4bf4304bce12219010ac4035f76c3a053654
unknown <rtiwarirahul123@gmail.com>
2023-04-03 01:00:49 +0530

    first commit for the app

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Rahul\Documents\FREELANCING\1.AppliedSkil\1.DEV\2.BACK_END\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
