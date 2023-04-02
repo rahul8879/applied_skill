af725896433a4cc85f1cff5b4e9b3e9af01ee0bc
unknown <rtiwarirahul123@gmail.com>
2023-04-03 02:09:45 +0530

    bug solved related to the html form

--- applied_skill/settings.py
+++ applied_skill/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8c-mii%x1%l_bd9pi&-2ucik5lfddi6z0dfs16uy2d-g8c$kb%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
