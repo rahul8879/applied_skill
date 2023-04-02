f059805efdc0171855aa449aea5444509ae59426
unknown <rtiwarirahul123@gmail.com>
2023-04-03 01:19:13 +0530

    static files added

---
+++ applied_skill/static/utils.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    // Show the modal after 15 seconds of page load
+    setTimeout(function() {
+      $('#enquiryModal').modal('show');
+    }, 60000);
+  
+    // Hide the modal and set a cookie when the cancel button is clicked
+    $('#enquiryModal button[data-dismiss="modal"]').click(function() {
+      // Set a cookie with a 1-day expiration
+      document.cookie = "enquiryModalShown=true; expires=" + new Date(new Date().getTime() + (24 * 60 * 60 * 1000)).toUTCString();
+  
+      // Hide the modal
+      $('#enquiryModal').modal('hide');
+    });
+  
+    // Check if the cookie exists and show/hide the modal accordingly
+    if (document.cookie.indexOf("enquiryModalShown=true") >= 0) {
+      // The cookie exists, hide the modal
+      $('#enquiryModal').modal('hide');
+    } else {
+      // The cookie doesn't exist, show the modal
+      $('#enquiryModal').modal('show');
+    }
+    // Hide the modal and set a cookie when the cancel button is clicked
+    $('#enquiryModal button[data-dismiss="modal"]').click(function() {
+        console.log("Cancel button clicked");
+        // Set a cookie with a 1-day expiration
+        document.cookie = "enquiryModalShown=true; expires=" + new Date(new Date().getTime() + (24 * 60 * 60 * 1000)).toUTCString();
+    });
+  });
+
+
+
